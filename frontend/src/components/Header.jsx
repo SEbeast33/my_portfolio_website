b86726cb9c758c4c0f3caf8472253407e1c16854
sharad <sharadetthar810@gmai.com>
2023-09-17 22:01:42 +0530

    hero section complete

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,30 +5,32 @@ import { motion } from "framer-motion"
 
 
 
+
 const Header = () => {
-  const svgVariants = {
-    hidden : { opacity: 0, fill: "transparent" },
-    visible:{opacity: 1,
-    transition :{duration :1 }}
-  }
-  const icon = {
-    hidden: {
-      pathLength: 0,
-      fill:" #FF5679"
-    },
-    visible: {
-      pathLength: 1,
-      fill: "#02A2AE"
-    }
-  }
+return (
 
 
-  return (
-    <div className='header_container'>
+  
+    <motion.div className='header_container'  whileInView={{ y: [-30, 0], opacity: [0, 1] }}
+    transition={{ duration: 0.8 }}>
         <div className='header'>
            <div className="brandlogo">
-            <img src={logo} alt="brand logo" />
-        
+
+            <motion.svg  width="50" height="39" viewBox="0 0 50 39" fill="none" xmlns="http://www.w3.org/2000/svg" className = "animate_fill"   initial={{ scale: 0 }}
+  animate={{ rotate: 360, scale: 1 }}
+  transition={{
+    type: "spring",
+    stiffness: 260,
+    damping: 20
+  }} > 
+            <g id="logo svg">
+<rect id="Rectangle8" width="43.9518" height="7.72211" rx="3.86105" transform="matrix(0.635688 -0.771946 0.754113 0.656745 0 33.9285)" fill="#02A2AE"  animate ={{rotate:360}} />
+<rect id="Rectangle9" width="27.4132" height="7.55639" rx="3.7782" transform="matrix(0.635688 -0.771946 0.754113 0.656745 19.1191 31.7476)" fill="#FF5679"/>
+<ellipse id="Ellipse2" cx="8.08846" cy="5.29311" rx="5.14706" ry="5.29311" fill="#FFAD14"/>
+<ellipse id="Ellipse3" cx="46.323" cy="6.80545" rx="3.67647" ry="3.78079" fill="#F9A101"/>
+</g>
+
+            </motion.svg>
              
             
             
@@ -37,17 +39,19 @@ const Header = () => {
             </div>
           </div>
           <div className="nav_components">
-            <div className="navitems"><a href="#">Home</a></div>
-            <div className="navitems"><a href="#">About us</a></div>
-            <div className="navitems"><a href="#">project</a></div>
-            <div className="navitems"><a href="#">Skills</a></div>
-            <div className="navitems"><a href="#">contact</a></div>
+            <motion.div className="navitems" whileHover={{scale :1.1,y:-5}}><a href="#">Home</a></motion.div>
+            <motion.div className="navitems" whileHover={{scale :1.1,y:-5}}><a href="#">About us</a></motion.div>
+            <motion.div className="navitems" whileHover={{scale :1.1,y:-5}}><a href="#">Projects</a></motion.div>
+            <motion.div className="navitems" whileHover={{scale :1.1,y:-5}}><a href="#">Skills</a></motion.div>
+            <motion.div className="navitems" whileHover={{scale :1.1,y:-5}}><a href="#">Contact</a></motion.div>
           </div>
           <div className="letstalkbutton">
-            <button className='lets_talk_button'>Let's Talk</button>
+            <motion.button className='lets_talk_button' whileHover={{scale : 1.1}}   whileTap={{ scale: 0.9 }}>Let's Talk</motion.button>
           </div>
         </div>
-    </div>
+    </motion.div>
+
+   
   )
 }
 
